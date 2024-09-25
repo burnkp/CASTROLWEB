0763dd8e7568ae44f46c23969b3a1b1dbfd16118
Kushtrim Puka <kushtrimpuka@gmail.com>
2024-09-25 22:33:43 +0000

    fixed supabase and product image loading in homepage

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY,
+  },
+  images: {
+    domains: ['yfsewjcuvvfhavpdugzr.supabase.co'],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
+}
 
 module.exports = nextConfig
